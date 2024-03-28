45c808d7acdcc045d8cf7497fcefb2adf42d76da
Clara <dhk0729@gmail.com>
2024-03-28 16:36:17 +0900

    ref를 사용해 input 데이터 가져오기

--- refs-and-portals-project/src/components/Player.jsx
+++ refs-and-portals-project/src/components/Player.jsx
@@ -1,10 +1,24 @@
+import { useState, useRef } from "react";
+
 export default function Player() {
+  const playerName = useRef();
+  const [text, setText] = useState(null);
+
+  const handleNameClick = () => {
+    // input 요소로 부터 바로 참조로 부터 값을 받는다.
+    setText(playerName.current.value);
+    playerName.current.value = '';
+  };
+
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>Welcome {text ?? 'unknown entity'}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input
+          ref={playerName}
+          type="text" />
+        <button onClick={handleNameClick}>Set Name</button>
       </p>
     </section>
   );
