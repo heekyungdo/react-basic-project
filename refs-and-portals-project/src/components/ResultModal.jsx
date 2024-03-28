aecca16df40b2c53ce53f55d56d851d683079ab1
Clara <dhk0729@gmail.com>
2024-03-28 20:01:39 +0900

    modal 완성

--- refs-and-portals-project/src/components/ResultModal.jsx
+++ refs-and-portals-project/src/components/ResultModal.jsx
@@ -5,6 +5,7 @@ const ResultModal = forwardRef(function ResultModal({ remainingTime, targetTime,
 
     const userLost = remainingTime <= 0;
     const formattedRaminingTime = (remainingTime / 1000).toFixed(2);
+    const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
 
     useImperativeHandle(ref, () => {
         return {
@@ -14,8 +15,11 @@ const ResultModal = forwardRef(function ResultModal({ remainingTime, targetTime,
         }
     })
 
-    return <dialog ref={dialog} className="result-modal">
+    // onClose={onReset} 속성은 esc로 닫을 경우. 이렇게 속성을 안줘도 esc키로 닫히긴 하지만,
+    // onReset함수가 트리거 되는 건 아니야. 
+    return <dialog ref={dialog} className="result-modal" onClose={onReset}>
         {userLost && <h2>Your lost</h2>}
+        {!userLost && <h2>Your Score: {score}</h2>}
         <p>The target time was <strong>{targetTime} seconds.</strong></p>
         <p>You stopped the timer with <strong>{formattedRaminingTime} seconds left.</strong></p>
         <form method="dialog" onSubmit={onReset}>
