3cc9e4064c179fa7d7be40fd3446444d20d6b184
Clara <dhk0729@gmail.com>
2024-04-03 22:37:16 +0900

    no project & new project 연결 (useState)

--- practice-project-instructor/src/App.jsx
+++ practice-project-instructor/src/App.jsx
@@ -1,11 +1,35 @@
-import NewProject from "./components/NewProject";
-import Sidebar from "./components/Sidebar";
+import { useState } from "react";
+import NewProject from "./components/NewProject.jsx";
+import Sidebar from "./components/Sidebar.jsx";
+import NoSelectedProject from "./components/NoSelectedProject.jsx"
 
 function App() {
+
+  const [projectsState, setProjectsState] = useState({
+    selectedId: undefined,
+    projects: []
+  });
+
+  function handleStartAddProject() {
+    setProjectsState((prevState) => {
+      return {
+        ...prevState,
+        selectedId: null
+      };
+    });
+  }
+
+  let content;
+  if (projectsState.selectedId === null) {
+    content = <NewProject />
+  } else {
+    content = <NoSelectedProject onStartAddProject={handleStartAddProject} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-    <Sidebar/>
-    <NewProject/>
+      <Sidebar onStartAddProject={handleStartAddProject} />
+      {content}
     </main>
   );
 }
--- practice-project-instructor/src/components/NoSelectedProject.jsx
+++ practice-project-instructor/src/components/NoSelectedProject.jsx
@@ -1,15 +1,15 @@
 import noProejct from '../assets/no-projects.png'
 import Button from './Button.jsx'
 
-export default function NoSelectedProject(){
+export default function NoSelectedProject({ onStartAddProject }) {
 
-    return(
+    return (
         <div className="mt-24 text-center w-2/3">
-            <img src={noProejct} alt='note image' className="w-16 h-16 object-contain mx-auto"/>
+            <img src={noProejct} alt='note image' className="w-16 h-16 object-contain mx-auto" />
             <h2 className="text-xl font-bold text-stone-500 my-4">No Project Selected</h2>
             <p className="text-stone-400 mb-4">Select a project or get started with a new on</p>
             <p className="mt-8">
-                <Button>Create new project</Button>
+                <Button onClick={onStartAddProject}>Create new project</Button>
             </p>
         </div>
     )
