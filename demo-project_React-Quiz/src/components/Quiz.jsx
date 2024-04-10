cfe3d26d7e9e058be6f53dbb52f219a1bb92b800
Clara <dhk0729@gmail.com>
2024-04-10 23:45:24 +0900

    답변 셔플

--- demo-project_React-Quiz/src/components/Quiz.jsx
+++ demo-project_React-Quiz/src/components/Quiz.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useRef, useState } from "react"
 
 import QUESTIONS from '../questions.js';
 
@@ -6,6 +6,14 @@ export default function Quiz() {
     const [userAnswers, setUserAnswers] = useState([]);
 
     const activeQuestionIndex = userAnswers.length;
+
+    if (QUESTIONS.length === activeQuestionIndex + 1) {
+
+    }
+
+    const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
+    // 100가지의 경우의 수 중에서 50개는 양수, 50개는 음수가 된다.
+    shuffledAnswers.sort(() => Math.random() - 0.5);
 
     function HandleSelectAnswer(selectedAnswer) {
         setUserAnswers((prevAnswers) => {
@@ -23,7 +31,7 @@ export default function Quiz() {
                     {QUESTIONS[activeQuestionIndex].text}
                 </h2>
                 <ul id="answers">
-                    {QUESTIONS[activeQuestionIndex].answers.map(answer =>
+                    {shuffledAnswers.map(answer =>
                         <li key={answer} className="answer">
                             <button onClick={() => HandleSelectAnswer(answer)}>
                                 {answer}
