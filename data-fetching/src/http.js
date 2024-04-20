de579d97a36239dcab06f036993af9fd60927fc8
Clara <dhk0729@gmail.com>
2024-04-20 16:36:14 +0900

    http get

--- data-fetching/src/http.js
+++ data-fetching/src/http.js
@@ -11,6 +11,18 @@ export async function fetchAvailablePlaces() {
     return resData.places;
 }
 
+export async function fetchUserPlaces() {
+    const response = await fetch('http://localhost:3000/user-places');
+    const resData = await response.json();
+
+    // 응답이 에러인지 아닌지 확인
+    // 만약 에러라면 (!response.ok)
+    if (!response.ok) {
+        throw new Error('Failed to fetch user places');
+    }
+
+    return resData.places;
+}
 
 export async function updateUserPlaces(places) {
     const response = await fetch('http://localhost:3000/user-places', {
