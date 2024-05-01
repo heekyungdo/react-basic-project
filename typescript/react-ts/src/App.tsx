2fdfd2f0fec17e233e67adbe79f61026c32ebd0a
Clara <dhk0729@gmail.com>
2024-05-02 00:31:06 +0900

    typescript - useState & 스타일 적용

--- typescript/react-ts/src/App.tsx
+++ typescript/react-ts/src/App.tsx
@@ -1,15 +1,24 @@
+import { useState } from 'react';
 import NewToDo from './components/NewTodo';
 import Todos from './components/Todos';
 import Todo from './models/todo';
 
 function App() {
-  const todos = [
-    new Todo('todo'),
-    new Todo('todo1'),
-  ];
+  // 아래와 같이 작성하면, todos가 never 타입이라고 나온다.
+  // never이란 타입은 배열이 언제나 비어있다는 의미. 어떤 값도 추가될 수 없다.
+  // const [todos, setTodos] = useState([]);
+
+  // 이렇게 써줘야한다. setState는 원래 제네릭타입이다.
+  // Todo[] => state가 Todo 배열을 관리한다는 의미이다.
+  // 처음에는 빈 배열이지만, 나중에 배열에 항목을 추가할 때는 반드시 Todo 타입이어야한다.
+  const [todos, setTodos] = useState<Todo[]>([]);
 
   const addTodoHandler = (todoText: string) => {
+    const newTodo = new Todo(todoText);
 
+    setTodos((prevTodos) => {
+      return prevTodos.concat(newTodo);
+    });
   };
 
   return (
--- typescript/react-ts/src/components/NewTodo.tsx
+++ typescript/react-ts/src/components/NewTodo.tsx
@@ -1,4 +1,6 @@
 import { useRef } from "react";
+import classes from './NewTodo.module.css'
+
 
 const NewToDo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     const todoText = useRef<HTMLInputElement>(null);
@@ -17,7 +19,9 @@ const NewToDo: React.FC<{ onAddTodo: (text: string) => void }> = (props) => {
     };
 
     return (
-        <form onSubmit={submitHandler}>
+        <form
+             onSubmit={submitHandler}
+            className={classes.form}>
             <label htmlFor="text">Todo Text</label>
             <input type="text" id="text" ref={todoText} />
             <button>Add Todo</button>
