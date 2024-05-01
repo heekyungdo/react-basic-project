a136554a25fb3340c4725213b5110266e799b8e3
Clara <dhk0729@gmail.com>
2024-05-01 15:13:04 +0900

    redux with useEffect

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -1,11 +1,23 @@
+import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
+  const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch('https://study-react-86263-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart)
+    });
+  }, [cart]);
+
   return (
     <Layout>
-      <Cart />
+      {showCart && <Cart />}
       <Products />
     </Layout>
   );
--- advanced-redux/src/components/Shop/Products.js
+++ advanced-redux/src/components/Shop/Products.js
@@ -1,16 +1,41 @@
 import ProductItem from './ProductItem';
 import classes from './Products.module.css';
 
+const DUMMY_PRODUCTS = [
+  {
+    id: 'p1',
+    title: 'First',
+    price: 6,
+    description: 'First book'
+  },
+  {
+    id: 'p2',
+    title: 'Second',
+    price: 12,
+    description: 'Second book'
+  },
+  {
+    id: 'p3',
+    title: 'Third',
+    price: 18,
+    description: 'Third book'
+  },
+]
+
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map(product =>
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        )}
       </ul>
     </section>
   );
