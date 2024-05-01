e1038fa0dc3f8bd4db2f01240586cf4fb2fcb865
Clara <dhk0729@gmail.com>
2024-05-01 16:39:50 +0900

    redux get data

--- advanced-redux/src/App.js
+++ advanced-redux/src/App.js
@@ -4,7 +4,9 @@ import Layout from './components/Layout/Layout';
 import Notification from './components/UI/Notification';
 import Products from './components/Shop/Products';
 import { useEffect } from 'react';
-import { uiActions } from './store/ui-slice';
+import { fetchCartData, sendCartData } from './store/cart-actions';
+
+let isInitial = true;
 
 function App() {
   const dispatch = useDispatch();
@@ -13,43 +15,17 @@ function App() {
   const notification = useSelector(state => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(
-        uiActions.showNotification({
-          status: 'pending',
-          title: 'Sending...',
-          message: 'Sending cart data!'
-        }));
-      const response = await fetch(
-        'https://study-react-86263-default-rtdb.asia-southeast1.firebasedatabase.app/cart.json',
-        {
-          method: 'PUT',
-          body: JSON.stringify(cart)
-        });
+    dispatch(fetchCartData());
+  }, [dispatch]);
 
-      // 에러
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.');
-      }
-
-      // 성공
-      dispatch(
-        uiActions.showNotification({
-          status: 'success',
-          title: 'Success!',
-          message: 'Sent cart data successfully!'
-        }));
+  useEffect(() => {
+    // !!isInitial일 때 장바구니 업데이트 X => 처음 렌더링될 때 빈 장바구니가 서버에 담기지 않도록 하기 위해.
+    if (isInitial) {
+      isInitial = false;
+      return;
     }
 
-       // 에러
-       sendCartData().catch(error => {
-        dispatch(
-          uiActions.showNotification({
-            status: 'error',
-            title: 'Error!',
-            message: 'Sending cart data failed'
-          }));
-      });
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
   return (
