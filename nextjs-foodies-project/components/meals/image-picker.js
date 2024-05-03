3a4549b7fb75b7297e4dd62f767a3f0ef132a446
Clara <dhk0729@gmail.com>
2024-05-04 02:28:01 +0900

    이미지 피커 미리보기

--- nextjs-foodies-project/components/meals/image-picker.js
+++ nextjs-foodies-project/components/meals/image-picker.js
@@ -1,26 +1,56 @@
 'use client';
 
-import { useRef } from 'react';
+import { useRef, useState } from 'react';
 import classes from './image-picker.module.css'
+import Image from 'next/image';
 
 export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
     const imageInput = useRef();
- 
+
     function handlePickClick() {
         imageInput.current.click();
+    }
+
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+
+        if (!file) {
+            // 이미지가 선택되지 않은 경우 미리보기된 이미지를 재설정
+            setPickedImage(null);
+            return;
+        }
+
+        const fileReader = new FileReader();
+
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result);
+        };
+
+        fileReader.readAsDataURL(file);
     }
 
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!pickedImage && <p>No image picked yet.</p>}
+                    {pickedImage &&
+                        <Image
+                            src={pickedImage}
+                            alt='The image selected by the user.'
+                            fill />}
+                </div>
                 <input
                     className={classes.input}
                     type='file'
                     id={name}
                     accept='image/png, image/jpeg'
                     name={name}
-                    ref={imageInput} />
+                    ref={imageInput}
+                    onChange={handleImageChange}
+                    required />
                 <button
                     className={classes.button}
                     type='button'
