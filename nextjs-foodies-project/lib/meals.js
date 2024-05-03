e949913398c2daad233b0c1def3497f0895fdc2d
Clara <dhk0729@gmail.com>
2024-05-04 01:32:46 +0900

    동적 페이지

--- nextjs-foodies-project/lib/meals.js
+++ nextjs-foodies-project/lib/meals.js
@@ -8,3 +8,9 @@ export async function getMeals() {
     // throw new Error('failed');
     return db.prepare('SELECT * FROM meals').all();
 }
+
+// 식사 정보의 메뉴를 식별하는 slug가 있어야한다. 
+// 플레이스홀더('?')에 들어가는 값을 get으로 전달한다.
+export function getMeal(slug) {
+    return db.prepare('SELECT * FROM meals WHERE slug = ?').get(slug);
+}
